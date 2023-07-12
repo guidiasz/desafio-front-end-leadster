b255225c378e5f526bc96c0b58468a80f5af6efd
Guilherme Dias <gui.diasz@hotmail.com>
2023-07-12 12:09:11 -0300

    fix: correct videos json url

--- api/getVideos.ts
+++ api/getVideos.ts
@@ -1,7 +1,7 @@
 import { VideoProps as Video } from '../src/context/VideosContext/interfaces';
 
 export const getVideos = async (): Promise<Video[]> => {
-  const response = await fetch('http://192.168.0.74:3000/videos.json');
+  const response = await fetch('https://leadster-challange.vercel.app/videos.json');
   const data = await response.json();
   return data.videos;
 };
