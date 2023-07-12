5ef83e05186ce35596e4ca14140f9d5f3e4e68f1
Guilherme Dias <gui.diasz@hotmail.com>
2023-07-11 22:44:58 -0300

    fix: title attribute for social icons

--- src/components/Contact/SocialSection.tsx
+++ src/components/Contact/SocialSection.tsx
@@ -9,16 +9,23 @@ const SocialSection: React.FC = () => {
         href="https://www.linkedin.com/company/getleadster/"
         target="_blank"
         rel="noopener noreferrer"
+        title="Linkedin"
       >
         <SocialIcon>{Linkedin}</SocialIcon>
       </a>
-      <a href="https://www.facebook.com/leadsterplatform" target="_blank" rel="noopener noreferrer">
+      <a
+        href="https://www.facebook.com/leadsterplatform"
+        target="_blank"
+        rel="noopener noreferrer"
+        title="Facebook"
+      >
         <SocialIcon>{Facebook}</SocialIcon>
       </a>
       <a
         href="https://www.instagram.com/leadster.com.br/"
         target="_blank"
         rel="noopener noreferrer"
+        title="Instagram"
       >
         <SocialIcon>{Instagram}</SocialIcon>
       </a>
